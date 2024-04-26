2e5bd76e04b9495d00fbac9509170dc594eda691
BuildTools <unconfigured@null.spigotmc.org>
2024-04-26 14:49:57 +0200

    Initial commit

---
+++ src/main/java/net/dongurs/delightfull/AnAdventurersDelightClient.java
@@ -0,0 +1,11 @@
+package net.dongurs.delightfull;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class AnAdventurersDelightClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+
+    }
+}
---
+++ src/main/java/net/dongurs/delightfull/AnAdventurersDelightDataGenerator.java
@@ -0,0 +1,11 @@
+package net.dongurs.delightfull;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class AnAdventurersDelightDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
